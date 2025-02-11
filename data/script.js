34be00ab32b0aa5243a3099426be956ef9a246c8
Richard190104 <119693579+Richard190104@users.noreply.github.com>
2025-02-11 12:13:06 +0100

    Update script.js

--- data/script.js
+++ data/script.js
@@ -366,6 +366,6 @@ if (window.innerWidth < 768){
     tresh = 0.1;
 }
 
-document.querySelector(".javascriptVersions").innerHTML = "Javascript verzia: 2.05"
+document.querySelector(".javascriptVersions").innerHTML = "Javascript verzia: 2.06"
 
 
