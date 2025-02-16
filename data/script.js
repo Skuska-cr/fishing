97762174264cda439f1f7300559e4554ea747bb9
Richard190104 <pukacrichard1@gmail.com>
2025-02-16 13:28:19 +0100

    3.02(H5C2J1)

--- data/script.js
+++ data/script.js
@@ -403,6 +403,6 @@ window.addEventListener("load", function() {
 
 });
 
-document.querySelector(".javascriptVersions").innerHTML = "App version: 3.02(H4C1J1)"
+document.querySelector(".javascriptVersions").innerHTML = "App version: 3.02(H5C2J1)"
 
 
