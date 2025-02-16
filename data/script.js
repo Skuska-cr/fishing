a7fa370d15a5ec41cfe52df794a9d876e1d83bdf
Richard190104 <pukacrichard1@gmail.com>
2025-02-16 12:46:25 +0100

    3.02 (H4C1J1)

--- data/script.js
+++ data/script.js
@@ -403,6 +403,6 @@ window.addEventListener("load", function() {
 
 });
 
-document.querySelector(".javascriptVersions").innerHTML = "Javascript verzia: 3.02"
+document.querySelector(".javascriptVersions").innerHTML = "App version: 3.02(H4C1J1)"
 
 
