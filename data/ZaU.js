7d0427b225684bd971546396bb4e4f505ce78f59
Richard190104 <pukacrichard1@gmail.com>
2025-02-17 20:42:29 +0100

    50.01(H0C1J1)

--- data/ZaU.js
+++ data/ZaU.js
@@ -13,10 +13,42 @@ buttons.forEach(button => {
 });
 
 var scrolled = 0;
+var zarb = document.querySelector('.zrbn');
+var ulvk = document.querySelector('.ulvk');
+
+zarb.addEventListener('click', function() {
+    if (scrolled == 0) {
+        zarb.classList.add('zau-base-visible');
+        zarb.classList.remove('zau-base-hidden');
+        ulvk.classList.add('zau-base-hidden');
+        ulvk.classList.remove('zau-base-visible');
+        var clcSwapElement = document.querySelector('.clcSwap');
+        var offset = clcSwapElement.offsetTop - 50; // Adjust the offset value as needed
+        window.scrollTo({ top: offset, behavior: 'smooth' });
+        console.log("neswitch")
+    }
+});
+
+ulvk.addEventListener('click', function() {
+    if (scrolled == 0) {
+        ulvk.classList.add('zau-base-visible');
+        ulvk.classList.remove('zau-base-hidden');
+        zarb.classList.add('zau-base-hidden');
+        zarb.classList.remove('zau-base-visible');
+        var clcSwapElement = document.querySelector('.clcSwap');
+        var offset = clcSwapElement.offsetTop - 50; // Adjust the offset value as needed
+        window.scrollTo({ top: offset, behavior: 'smooth' });
+        console.log("neswitch")
+    }
+});
+
+
 
 document.addEventListener("scroll", function() {
+    console.log(scrolled)
     var scroll = document.documentElement.scrollTop;
     var visible = document.querySelector('.zau-base-visible') || document.querySelector('.zau-visible');
+    console.log(visible)
     var hidden = document.querySelector('.zau-base-hidden') || document.querySelector('.zau-hidden');
     if (scroll > 100 && scrolled == 0) {
         scrolled = 1;
