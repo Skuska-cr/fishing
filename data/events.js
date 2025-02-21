c9836e800ef9f059f6876b0eaa75c21eb9a9927f
Richard190104 <pukacrichard1@gmail.com>
2025-02-21 15:45:12 +0100

    SmallUpdt

--- data/events.js
+++ data/events.js
@@ -1,20 +1,39 @@
 
-var jsonUrl = "https://raw.githubusercontent.com/Richard190104/fishing/refs/heads/main/data/pdfs.json";
+function createPageContent(event){
+    var pdjInnerElemet = document.querySelector(".pdjInnerElement");
+    pdjInnerElemet.innerHTML = "";
+    var head = document.createElement("h2");
+    var text = document.createElement("p");
+    console.log(pdjInnerElemet)
+    head.innerHTML = event.name;
+    text.innerHTML = event.text;
+    pdjInnerElemet.appendChild(head);
+    pdjInnerElemet.appendChild(text);
+}
+
+
+function generatePage(event){
+    var List = document.querySelector(".pdjList");
+    var listValue = document.createElement("h3");
+    listValue.innerHTML = event.name;
+    List.appendChild(listValue);
+    listValue.addEventListener('click', () => {
+        createPageContent(event);
+    });
+}
+
+
+var jsonUrl = "https://raw.githubusercontent.com/Richard190104/fishing/refs/heads/main/data/events.json";
 
 
 fetch(jsonUrl)
 .then(response => response.json())
 .then(data => {
     
-    Object.keys(data.z).forEach(year => {
-        const pdfItem = createPdfItem(data.z[year],year);
+    Object.keys(data).forEach(event => {
+        generatePage(data[event]);
         
-        document.querySelectorAll(".zau-box-content")[0].appendChild(pdfItem);
     });
-    Object.keys(data.u).forEach(year => {
-        const pdfItem = createPdfItem(data.u[year],year);
-        
-        document.querySelectorAll(".zau-box-content")[1].appendChild(pdfItem);
-    });
+   
 
 });
