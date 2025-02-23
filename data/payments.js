6e2cadc0777e91064dd3a5fe6459848b49b05d42
Richard190104 <pukacrichard1@gmail.com>
2025-02-23 15:37:19 +0100

    pre-7.0

---
+++ data/payments.js
@@ -0,0 +1,25 @@
+
+var jsonUrl = "https://raw.githubusercontent.com/Richard190104/fishing/refs/heads/main/data/jsons/payments.json";
+const container = document.querySelector(".ppz-manContainer");
+
+fetch(jsonUrl)
+.then(response => response.json())
+.then(data => {
+    
+    Object.keys(data).forEach(value => {
+        createElement(value);
+        
+    });
+   
+    var pdjInnerElemet = document.querySelector(".pdjInnerElement");
+    pdjInnerElemet.style.left = "0px";
+});
+
+function createElement(value){
+    var div = document.createElement("div");
+    div.classList.add("ppz-block");
+    var head = document.createElement("h2");
+    head.innerHTML = value.name;
+    div.appendChild(head);
+    container.appendChild(div);
+}
